a5b0898b62e52f5f8ebe5e90b2f7c2b965839699
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2023-02-06 23:18:47 -0500

    added a sample image for adjustment kit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import { Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
 import Set1 from './components/Set1';
 import Set2 from './components/Set2';
 import Set3 from './components/Set3';
-
-
 import './App.css';
 
 function App() {
@@ -14,6 +13,7 @@ function App() {
       <Link to='/set2'>Set2</Link>
       <Link to='/set3'>Set3</Link>
       <Routes>
+        <Route path="/" element={<Home />}></Route>
         <Route path="/set1" element={<Set1 />}></Route>
         <Route path="/set2" element={<Set2 />}></Route>
         <Route path="/set3" element={<Set3 />}></Route>
--- src/components/Set1.tsx
+++ src/components/Set1.tsx
@@ -1,7 +1,10 @@
+import adjustmentKit from '../images/adjustmentKit.jpg'
+
 export default function Set1(){
     return (
         <div>
             <h1>This is set 1</h1>
+            <img src={adjustmentKit} alt='adjustment kit'></img>
         </div>
     )
 }
