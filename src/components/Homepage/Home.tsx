c259c217bcaadf9adc22b9e9ab09faf297941224
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2023-03-03 13:36:14 -0500

    re-desgined the landing page

--- src/components/Homepage/Home.tsx
+++ src/components/Homepage/Home.tsx
@@ -1,16 +1,33 @@
 import Content from "./Content"
+import landingPage from "../images/landingPage.jpg";
 
 export default function Home(){
     return (
         <div className="main-page">
-            <div className="hero-msg">
-                <h1 id="hero">Welcome to Orthodontic Harmony!</h1>
-                <h3 id="hero">Explore instrument set ups for Orthodontist.</h3>
-                <Content />
+
+
+            <div className="hero-background" style={{background: `url(${landingPage})`,
+                backgroundRepeat:'no-repeat',
+                backgroundSize:'cover',
+                backgroundColor: 'grey',
+                backgroundBlendMode: 'multiply'
+            }}>
+            <div className="hero-msg" >
+                <h1 id="hero" style={{fontSize:'90px'}}>Welcome to Orthodontic Harmony!</h1>
+                <h4 id="hero" style={{fontSize:'50px'}}>Explore instrument set ups for Orthodontist.</h4>
+
+                <div className="arrow-container">
+                    <div className="arrow">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+            </div>
             </div>
 
             <div className="table-content-container">
-                
+                <Content />
             </div>
 
             
