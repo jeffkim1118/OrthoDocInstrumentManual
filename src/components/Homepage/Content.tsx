db3ad872213d0995353d806fc7bc8c47707aaaa9
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2023-03-08 00:19:42 -0500

    background gradient added for each kit pages and also added a DOM loader circle

--- src/components/Homepage/Content.tsx
+++ src/components/Homepage/Content.tsx
@@ -5,7 +5,7 @@ import setUps from "./Setups";
 export default function Content() {
   return (
     <div id="content" className="content">
-      <h2 style={{color:'whitesmoke',textAlign:'center', padding:'100px'}}><em>Here are our necessary kits</em></h2>
+      <h2 style={{color:'whitesmoke',textAlign:'center', padding:'70px'}}><em>Here are our necessary kits</em></h2>
       
       {setUps.map((setUp,indx) => {
         return <Card setUp={setUp} indx={indx} key={indx}/>
