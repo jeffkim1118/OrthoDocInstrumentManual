c33b6fd6a9927cfc2bad664cacc216f70fa1639e
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2023-03-25 12:17:55 -0400

    redesigned the login page

--- src/components/Account/Login.tsx
+++ src/components/Account/Login.tsx
@@ -1,60 +1,86 @@
-import {useState} from 'react';
+import { useState } from "react";
 import { Button } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import { useSelector,useDispatch } from 'react-redux';
-import { selectUser,login } from '../../features/userSlice';
+import { useSelector, useDispatch } from "react-redux";
+import { selectUser, login } from "../../features/userSlice";
 
 export default function Login() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  
-  const handleLogin = (e:any) => {
+
+  const handleLogin = (e: any) => {
     e.preventDefault();
     const loggingInUser = {
       username,
-      password
-    }
+      password,
+    };
 
     fetch(`http://localhost:3000/login`, {
-      method:"POST",
+      method: "POST",
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
+        Accept: "application/json",
+        "Content-Type": "application/json",
       },
-      body:JSON.stringify(loggingInUser)
+      body: JSON.stringify(loggingInUser),
     })
-    .then(res => res.json())
-    .then(data => {
-      localStorage.setItem('token', data.token);
-      dispatch(login(data.user))
-    })
-    navigate('/profile')
-  }
+      .then((res) => res.json())
+      .then((data) => {
+        localStorage.setItem("token", data.token);
+        dispatch(login(data.user));
+      });
+    navigate("/profile");
+  };
   return (
-    <div className="background">
-      <div className="login-container" style={{padding:'250px'}}>
-        <form
-          className="login-form"
-          style={{ display: "flex", flexDirection: "column", width: "500px", backgroundColor:'white', margin:'auto',borderRadius:'10px',padding:'35px', boxShadow:'8px 7px 5px 5px black'}}
-          onSubmit={(e) => handleLogin(e)}
-        >  
-            <h1 style={{margin:'auto'}}><em>Login</em></h1>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Username</Form.Label>
-            <Form.Control type="text" placeholder="Enter Username" value={username} onChange={(e) => setUsername(e.target.value)}></Form.Control>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-          </Form.Group>
-          <Button variant="primary" type="submit">Login</Button>
-          <Link to={'/signup'} style={{padding:'10px',margin:'auto'}}>Don't have an account?</Link>
-          <Link to={'/'} style={{margin:'auto'}}>Forgot your password?</Link>
-        </form>
+    <div className="form-container">
+      <div className="form d-md-flex align-items-center justify-content-between">
+        <div className="box-1 mt-md-0 mt-5">
+          <img
+            src="https://images.pexels.com/photos/2033997/pexels-photo-2033997.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            className=""
+            alt=""
+          />
+        </div>
+
+        <div className="box-2 d-flex flex-column h-100">
+          <div className="mt-5" />
+          <form className="login-form" onSubmit={(e) => handleLogin(e)}>
+            <h1 style={{ margin: "auto" }}>
+              <em>Login</em>
+            </h1>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
+
+            <Button variant="primary" type="submit" style={{marginTop:'10px', marginBottom:'10px'}}>
+              Login
+            </Button><br/>
+            <Link to={"/signup"} >
+              Don't have an account?
+            </Link><br/>
+            <Link to={"/"}>
+              Forgot your password?
+            </Link>
+          </form>
+        </div>
       </div>
     </div>
   );
