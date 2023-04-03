d609b82f54fbd9622b382e72b591aeacb7e79474
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2023-04-02 21:10:09 -0400

    testing out react-final-form

--- src/components/Account/Login.tsx
+++ src/components/Account/Login.tsx
@@ -1,91 +1,167 @@
-import { useState } from "react";
-import { Button } from "react-bootstrap";
-import Form from "react-bootstrap/Form";
-import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { login } from "../../features/userSlice";
-import accountIcon from "../../components/images/account/account.png";
-import padLock from "../../components/images/account/padlock.png";
+// import { useState } from "react";
+// import { Button } from "react-bootstrap";
+// // import Form from "react-bootstrap/Form";
+// import { Link } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
+// import { useDispatch } from "react-redux";
+// import { login } from "../../features/userSlice";
+// import accountIcon from "../../components/images/account/account.png";
+// import padLock from "../../components/images/account/padlock.png";
+import { Form, Field } from "react-final-form";
 
 export default function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  // const [username, setUsername] = useState("");
+  // const [password, setPassword] = useState("");
 
-  const [verifyUsername, setVerifyUsername] = useState(true);
-  const [verifyPassword, setVerifyPassword] = useState(true);
-  const [invalidAccount, checkAccount] = useState(true);
+  // const [verifyUsername, setVerifyUsername] = useState(true);
+  // const [verifyPassword, setVerifyPassword] = useState(true);
+  // const [invalidAccount, checkAccount] = useState(true);
 
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
+  // const navigate = useNavigate();
+  // const dispatch = useDispatch();
 
-  const handleLogin = (e: any) => {
-    e.preventDefault();
-    if (username && password) {
-      const loggingInUser = {
-        username,
-        password,
-      };
+  // const handleLogin = (e: any) => {
+  //   e.preventDefault();
+  //   if (username && password) {
+  //     const loggingInUser = {
+  //       username,
+  //       password,
+  //     };
 
-      fetch(`http://localhost:3000/login`, {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(loggingInUser),
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          localStorage.setItem("token", data.token);
-          dispatch(login(data.user));
-          navigate("/profile");
-        })
-        .catch(error => {
-          console.log(error)
-          checkAccount(false)
-        });
+  //     fetch(`http://localhost:3000/login`, {
+  //       method: "POST",
+  //       headers: {
+  //         Accept: "application/json",
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify(loggingInUser),
+  //     })
+  //       .then((res) => res.json())
+  //       .then((data) => {
+  //         localStorage.setItem("token", data.token);
+  //         dispatch(login(data.user));
+  //         navigate("/profile");
+  //       })
+  //       .catch((error) => {
+  //         console.log(error);
+  //         checkAccount(false);
+  //       });
+  //   } else if (!username && !password) {
+  //     setVerifyUsername(false);
+  //     setVerifyPassword(false);
+  //   } else if (!username) {
+  //     setVerifyUsername(false);
+  //   } else if (!password) {
+  //     setVerifyPassword(false);
+  //   }
+  // };
 
-    } else if (!username && !password){
-      setVerifyUsername(false);
-      setVerifyPassword(false);
-    } else if (!username) {
-      setVerifyUsername(false);
-    } else if (!password) {
-      setVerifyPassword(false);
-    }
+  // const handleBlur = () => {
+  //   const usernameInput: any =
+  //     document.getElementsByClassName("username-input")[0];
+  //   const passwordInput: any =
+  //     document.getElementsByClassName("password-input")[0];
+  //   if (!usernameInput.value) {
+  //     usernameInput.style.border = "1px solid red";
+  //     setVerifyUsername(false);
+  //   }
+  //   if (!passwordInput.value) {
+  //     passwordInput.style.border = "1px solid red";
+  //     setVerifyPassword(false);
+  //   }
+  // };
+
+  // const handleFocus = () => {
+  //   const usernameInput: any =
+  //     document.getElementsByClassName("username-input")[0];
+  //   const passwordInput: any =
+  //     document.getElementsByClassName("password-input")[0];
+  //   if (usernameInput.value) {
+  //     usernameInput.style.border = "1px solid black";
+  //     setVerifyUsername(true);
+  //   }
+  //   if (passwordInput.value) {
+  //     passwordInput.style.border = "1px solid black";
+  //     setVerifyPassword(true);
+  //   }
+  // };
+
+  const handleSubmit = async (values:any) => {
+    window.alert(JSON.stringify(values,undefined,2))
   };
 
-  const handleBlur = () => {
-    const usernameInput:any = document.getElementsByClassName('username-input')[0];
-    const passwordInput:any = document.getElementsByClassName('password-input')[0];
-    if(!usernameInput.value){
-      usernameInput.style.border = '1px solid red'
-      setVerifyUsername(false);
-    }
-    if(!passwordInput.value){
-      passwordInput.style.border = '1px solid red'
-      setVerifyPassword(false);
-    }
-  }
-
-  const handleFocus = () => {
-    const usernameInput:any = document.getElementsByClassName('username-input')[0];
-    const passwordInput:any = document.getElementsByClassName('password-input')[0];
-    if(usernameInput.value){
-      usernameInput.style.border = '1px solid black';
-      setVerifyUsername(true);
-    }
-    if(passwordInput.value){
-      passwordInput.style.border = '1px solid black'
-      setVerifyPassword(true);
-    }
-  }
+  const mustBeNumber = (value:any) =>
+    isNaN(value) ? "Must be a number" : undefined;
+  const minValue = (min:any) => (value:any) =>
+    isNaN(value) || value >= min ? undefined : `Should be greater than ${min}`;
+  const composeValidators =
+    (...validators:any) =>
+    (value:any) =>
+      validators.reduce(
+        (error:any, validator:any) => error || validator(value),
+        undefined
+      );
+  const required = (value: any) => (value ? undefined : "Required");
 
   return (
     <div className="background">
       <div className="form-container">
-        <div className="form d-md-flex align-items-center justify-content-between">
+        <Form
+          onSubmit={handleSubmit}
+          render={({ handleSubmit, form, submitting, pristine, values }) => (
+            <form onSubmit={handleSubmit}>
+              <Field name="firstName" validate={required}>
+                {({ input, meta }) => (
+                  <div>
+                    <label>First Name</label>
+                    <input {...input} type="text" placeholder="First Name" />
+                    {meta.error && meta.touched && <span>{meta.error}</span>}
+                  </div>
+                )}
+              </Field>
+              <Field name="lastName" validate={required}>
+                {({ input, meta }) => (
+                  <div>
+                    <label>Last Name</label>
+                    <input {...input} type="text" placeholder="Last Name" />
+                    {meta.error && meta.touched && <span>{meta.error}</span>}
+                  </div>
+                )}
+              </Field>
+              <Field
+                name="age"
+                validate={composeValidators(
+                  required,
+                  mustBeNumber,
+                  minValue(18)
+                )}
+              >
+                {({ input, meta }) => (
+                  <div>
+                    <label>Age</label>
+                    <input {...input} type="text" placeholder="Age" />
+                    {meta.error && meta.touched && <span>{meta.error}</span>}
+                  </div>
+                )}
+              </Field>
+              <div className="buttons">
+                <button type="submit" disabled={submitting}>
+                  Submit
+                </button>
+                <button
+                  type="button"
+                  onClick={form.reset}
+                  disabled={submitting || pristine}
+                >
+                  Reset
+                </button>
+              </div>
+              <pre>{JSON.stringify(values,undefined,2)}</pre>
+            </form>
+          )}
+        />
+
+        {/* <div className="form d-md-flex align-items-center justify-content-between">
           <div className="box-1 mt-md-0 mt-5">
             <img
               src="https://images.pexels.com/photos/2033997/pexels-photo-2033997.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
@@ -149,6 +225,7 @@ export default function Login() {
                 {!verifyPassword ? (<p style={{ fontSize: "12px", color:'red', margin:'0px',border:'none',padding:'none' }}>Invalid password</p>) : null}
               </Form.Group>
               <Button
+                className="login-btn"
                 variant="primary"
                 type="submit"
                 style={{
@@ -167,86 +244,8 @@ export default function Login() {
               </div>
             </form>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
 }
-
-// import { useState } from "react";
-// import LoginForm from "./Login-Form";
-
-// const Login = () => {
-//   const navigate = useNavigate();
-//   const dispatch = useDispatch();
-
-//   const [values, setValues] = useState({
-//     username: "",
-//     password: "",
-//   });
-
-//   const inputs = [
-//     {
-//       id: 1,
-//       name: "username",
-//       type: "text",
-//       placeholder: "Username",
-//       errorMessage:
-//         "Username should be 3-16 characters and shouldn't include any special character!",
-//       label: "Username",
-//       pattern: "^[A-Za-z0-9]{3,16}$",
-//       required: true,
-//     },
-//     {
-//       id: 2,
-//       name: "password",
-//       type: "password",
-//       placeholder: "Password",
-//       errorMessage:
-//         "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
-//       label: "Password",
-//       pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
-//       required: true,
-//     },
-//   ];
-
-//   const handleSubmit = (e: any) => {
-//     e.preventDefault();
-//     if (values.username && values.password) {
-//       fetch(`http://localhost:3000/login`, {
-//         method: "POST",
-//         headers: {
-//           Accept: "application/json",
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify(values),
-//       })
-//         .then((res) => res.json())
-//         .then((data) => {
-//           localStorage.setItem("token", data.token);
-//           dispatch(login(data.user));
-//           navigate("/profile");
-//         })
-//         .catch((error) => {
-//           console.log(error);
-//         });
-//     }
-
-//     const onChange = (e: any) => {
-//       setValues({ ...values, [e.target.name]: e.target.value });
-//     };
-
-//     return (
-//       <div className="app">
-//         <form onSubmit={handleSubmit}>
-//           <h1>Register</h1>
-//           {inputs.map((input) => (
-//             <LoginForm key={input.id} {...input} onChange={onChange} />
-//           ))}
-//           <button>Submit</button>
-//         </form>
-//       </div>
-//     );
-//   };
-// };
-// export default Login;
