503c521971f27b4cdbcde8e8b096af7e6783e385
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2023-07-23 16:11:19 -0400

    commented out user's image in chat to prevent load failure

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -9,18 +9,18 @@ export default function Chat({ msg, decodedUser }: any) {
   const user: any = useSelector(selectUser);
   const { store } = useContext(ReactReduxContext)
   const currentUserData = store.getState().user;
-
+  
   return (
     <div className="chatbox-container">
       <div
         className={msg.user_id === decodedUser.id ? "mychat" : "otherchat"}
         key={msg.i}
       >
-        <img
-          src={user.avatar_url? user.avatar_url : null}
-          alt="profile-img"
+        {/* <img
+          src={user.avatar_url}
+          alt={profileImage}
           className="chat-profile-avatar"
-        ></img>
+        ></img> */}
 
         <span>{msg.username}</span>
         <br />
