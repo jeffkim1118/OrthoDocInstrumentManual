fb7d3b05cfacf64a9584a1eb106c7a9728ad8715
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2023-09-13 21:12:02 -0400

    working on creating a search page for instrument setups

--- src/components/SetupSearchBar.tsx
+++ src/components/SetupSearchBar.tsx
@@ -1,32 +1,45 @@
-import Form from 'react-bootstrap/Form';
-import obj from './Instruments';
-import {useState, useEffect} from 'react'
-export default function SetupSearchBar(){
+import Form from "react-bootstrap/Form";
+import obj from "./Instruments";
+import { useState, useEffect } from "react";
+import Nav from 'react-bootstrap/Nav';
+import setUps from "./Homepage/Setups";
 
-    let setupArray = ["setup1", "setup2", "setup3", "setup4", "setup5"];
+export default function SetupSearchBar() {
+  let setupArray = [ "setup3", "setup4", "setup5"];
 
-    const [searchList, setSearchList] = useState(setupArray);
-    const [searchQuery, setSearchQuery] = useState("");
+  let testArray = []
 
-    useEffect(()=>{
-        if (searchQuery === "") { setSearchList(setupArray); return; }
-        const filterBySearch = setupArray.filter((setup) => {
-            if (setup.toLowerCase()
-                .includes(searchQuery.toLowerCase())) { return setup; }
-        })
-        setSearchList(filterBySearch);
-    },[searchQuery])
+  const [searchList, setSearchList] = useState(setupArray);
+  const [searchQuery, setSearchQuery] = useState("");
 
-    return(
+  useEffect(() => {
+    if (searchQuery === "") {
+      setSearchList(setupArray);
+      return;
+    }
+    const filterBySearch = setupArray.filter((setup) => {
+      if (setup.toLowerCase().includes(searchQuery.toLowerCase())) {
+        return setup;
+      }
+    });
+    setSearchList(filterBySearch);
+  }, [searchQuery]);
+
+  return (
+    <div className="setUpSearchBarPage">
+      <div className="setUpSearchBarContainer">
+        <input
+          type="text"
+          value={searchQuery}
+          placeholder="Search here"
+          onChange={(e: any) => setSearchQuery(e.target.value)}
+        ></input>
         <div>
-            <input type='text' value={searchQuery} onChange={(e:any) => setSearchQuery(e.target.value)}></input>
-            <div>
-                {searchList.map((setup) => {
-                    return(
-                        <div>{setup}</div>
-                    )
-                })}
-            </div>
+          {searchList.map((setup) => {
+            return <div className="searchResultItem"><Nav.Link>{setup}</Nav.Link></div>;
+          })}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
