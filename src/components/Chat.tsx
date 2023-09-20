cdb9e8e7f1c93e7f368de27df30db68407ebf7b1
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2023-09-19 21:06:27 -0400

    working on how to display messages grouped by dates that the messages were created

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -3,13 +3,13 @@ import Moment from "moment";
 import { selectUser } from "../features/userSlice";
 import { useSelector } from "react-redux";
 import profileImage from "./images/account/defaultProfilePicture.png";
-import { ReactReduxContext } from 'react-redux'
+import { ReactReduxContext } from "react-redux";
 
 export default function Chat({ msg, decodedUser }: any) {
   const user: any = useSelector(selectUser);
-  const { store } = useContext(ReactReduxContext)
+  const { store } = useContext(ReactReduxContext);
   const currentUserData = store.getState().user;
-  
+  console.log(currentUserData.user.username);
   return (
     <div className="chatbox-container">
       <div
@@ -22,13 +22,17 @@ export default function Chat({ msg, decodedUser }: any) {
           className="chat-profile-avatar"
         ></img> */}
 
-        <span>{msg.username}</span>
+        {currentUserData.user.username === msg.username ? null : (
+          <span>{msg.username}</span>
+        )}
         <br />
         <span>{msg.content}</span>
         <br />
-        <span className="msg-time">
-          {Moment(msg.created_at).format("MMMM Do YYYY, h:mm:ss a")}
-        </span>
+        <div className="date-time-indicator">
+          <span className="msg-time">
+            {Moment(msg.created_at).format("h:mm:ss a")}
+          </span>
+        </div>
       </div>
     </div>
   );
