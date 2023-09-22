b32baf2118acfbdaab41ea75f699d2258f4bdb51
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2023-09-22 19:36:37 -0400

    working on re-designing setup pages

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -1,21 +1,23 @@
 import obj from "./Instruments";
 import ModalP from "./ModalPopup/ModalP";
 
-function Page(){
-    const information = obj[window.location.pathname];
-    const {name, image, desc} = information
-    
-    return(
-        <div id="kit-container">
-            <div className="d-flex flex-column justify-content-center w-100 h-100"></div>
-            <div id="kit">
-                <h1>{name}</h1>
-                <div className="test">
-                    {image.map((x:any) => (<ModalP v={x}/>))}
-                </div>
-                <p>{desc}</p>
-            </div>
+function Page() {
+  const information = obj[window.location.pathname];
+  const { name, image, desc } = information;
+
+  return (
+    <div id="kit-container">
+      <div className="kit">
+        <h1>{name}</h1>
+        <div className="setup-picture-container">
+          {image.map((x: any) => (
+            <ModalP className="setup-picture" v={x} />
+          ))}
         </div>
-    )
+
+        <p>{desc}</p>
+      </div>
+    </div>
+  );
 }
 export default Page;
