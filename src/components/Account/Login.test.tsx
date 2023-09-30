ab5f33c6709bbd8e559a60e508d612f76aa6c02a
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2023-09-30 16:41:11 -0400

    unit test for login component is made

---
+++ src/components/Account/Login.test.tsx
@@ -0,0 +1,21 @@
+import Login from "./Login";
+import Profile from "../Profile/Profile";
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect'; // You can extend the expect function with jest-dom
+
+test("Login button should take the user to profile page after a successful authentication", async () => {
+  render(<Login />);
+  const loginBtn = screen.getByLabelText("Submit");
+  const username = screen.getByPlaceholderText("Username");
+  const password = screen.getByPlaceholderText("Password");
+
+  fireEvent.change(username, { target: { value: "first_user" } });
+  fireEvent.change(password, { target: { value: "password" } });
+
+  fireEvent.click(loginBtn);
+
+  // Check if the Profile component is rendered
+  const profileComponent = screen.getByText("Profile"); // Assuming there's some text like "Profile" on the Profile component
+  expect(profileComponent).toBeInTheDocument();
+});
