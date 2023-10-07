a9080931e91ce932056caceef28431afeed3a8ee
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2023-10-06 20:15:37 -0400

    start writing test cases for Modal Popup component

---
+++ src/components/ModalPopup/ModalP.test.tsx
@@ -0,0 +1,8 @@
+import ModalP from "./ModalP";
+import { render } from "@testing-library/react";
+
+describe("The modal popup component", () => {
+    it("should render with different photos based on setups that are provided", () => {
+        
+    })
+})
