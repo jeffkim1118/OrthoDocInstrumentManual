6bb5b87c873ef1fff868a694f7b6cebee7c092ca
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2023-10-07 13:18:23 -0400

    added a test to check if modal popup renders properly with an image

--- src/components/ModalPopup/ModalP.test.tsx
+++ src/components/ModalPopup/ModalP.test.tsx
@@ -1,8 +1,13 @@
 import ModalP from "./ModalP";
 import { render } from "@testing-library/react";
+import Modal from 'react-bootstrap/Modal';
+import AdjustmentPic from '../images/adjustment/adjustmentKit.jpg';
+
+const testSample = AdjustmentPic;
 
 describe("The modal popup component", () => {
     it("should render with different photos based on setups that are provided", () => {
-        
+        render(<ModalP image={testSample}/>)
+        expect(render(<ModalP/>))
     })
 })
